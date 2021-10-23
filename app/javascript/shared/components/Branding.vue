f178813b5d514ca069166edd8fe1848d22b68655
Duc Hieu <hieund.uet@gmail.com>
2021-10-23 13:35:45 +0700

    update

--- app/javascript/shared/components/Branding.vue
+++ app/javascript/shared/components/Branding.vue
@@ -1,14 +1,14 @@
 <template>
   <div v-if="globalConfig.brandName" class="branding">
     <a
-      href="javascript:;"
+      :href="brandRedirectURL"
       rel="noreferrer noopener nofollow"
       target="_blank"
       class="branding--link w-full justify-center"
     >
       <img :alt="globalConfig.brandName" :src="globalConfig.logoThumbnail" />
       <span>
-        ChatXXX
+        {{ useInstallationName($t('POWERED_BY'), globalConfig.brandName) }}
       </span>
     </a>
   </div>
